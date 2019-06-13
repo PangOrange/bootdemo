2b9982ea978ac3b8e6aa4a9e9c6127873613c12c
chenglintong <993979434@qq.com>
2019-06-13 15:39:35 +0800

    拦截器测试

--- src/main/java/com/orange/bootdemo/BootdemoApplication.java
+++ src/main/java/com/orange/bootdemo/BootdemoApplication.java
@@ -1,12 +1,22 @@
 package com.orange.bootdemo;
 
+import com.orange.bootdemo.handle.Interceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BootdemoApplication {
+public class BootdemoApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {SpringApplication.run(BootdemoApplication.class, args);}
 
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        //注册拦截器
+        InterceptorRegistration interceptorRegistration = registry.addInterceptor(new Interceptor());
+        interceptorRegistration.addPathPatterns("/interceptor");
+    }
 }
